7e56d54e3b7eaa470864d3620539a59db9cc1738
Viniarte <vinicius.trink@gmail.com>
2025-06-27 02:21:33 +0900

    Instalando dependências NPM e Composer com sucesso

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'viniarte/funnelcraft-base',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'f05ea7a2c865fca6473bc2034c75ad7eb4aa5117',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'viniarte/funnelcraft-base' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'f05ea7a2c865fca6473bc2034c75ad7eb4aa5117',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
